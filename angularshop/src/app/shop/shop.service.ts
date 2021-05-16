2d143dd6cf9cc837b5dd1876edfe2259477b88ca
esilean <le.bevilaqua@gmail.com>
2021-05-16 20:19:41 -0300

    feat: finish shop page

--- angularshop/src/app/shop/shop.service.ts
+++ angularshop/src/app/shop/shop.service.ts
@@ -1,6 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
 import { Pagination } from '../shared/models/pagination';
+import { ProductBrand } from '../shared/models/product-brand';
+import { ProductType } from '../shared/models/product-type';
+import { ShopParams } from '../shared/models/shop-params';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +15,42 @@ export class ShopService {
 
   constructor(private http: HttpClient) {}
 
-  getProducts() {
-    return this.http.get<Pagination>(this.baseUrl + 'products');
+  getProducts(shopParams: ShopParams) {
+    let params = new HttpParams();
+
+    if (shopParams.brandId !== 0) {
+      params = params.append('brandId', shopParams.brandId.toString());
+    }
+
+    if (shopParams.typeId !== 0) {
+      params = params.append('typeId', shopParams.typeId.toString());
+    }
+
+    if (shopParams.search) {
+      params = params.append('search', shopParams.search);
+    }
+
+    params = params.append('sort', shopParams.sort);
+    params = params.append('pageIndex', shopParams.pageNumber.toString());
+    params = params.append('pageSize', shopParams.pageSize.toString());
+
+    return this.http
+      .get<Pagination>(this.baseUrl + 'products', {
+        observe: 'response',
+        params,
+      })
+      .pipe(
+        map((response) => {
+          return response.body;
+        })
+      );
+  }
+
+  getBrands() {
+    return this.http.get<ProductBrand[]>(this.baseUrl + 'products/brands');
+  }
+
+  getTypes() {
+    return this.http.get<ProductType[]>(this.baseUrl + 'products/types');
   }
 }
